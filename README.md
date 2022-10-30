e542da11946feedbc07f96be28e94640d01dddaf
Bosshentai <baptistamhernani@gmail.com>
2022-10-30 16:52:44 -0100

    add the obsidian config

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Android-Uni
  
+| Nome    | Descrição                                               | Link                                                                           |
+| ------- | ------------------------------------------------------- | ------------------------------------------------------------------------------ |
+| TP_1.1  | Apresenter sobre Android studio e criar Hello World app | https://developer.android.com/codelabs/android-training-hello-world#3          |
+| TP_1.2A | O teu Primeiro UI interativo                            | https://developer.android.com/codelabs/android-training-layout-editor-part-a   |
+| TP_1.2B | O Editor de Layout                                      | https://developer.android.com/codelabs/android-training-layout-editor-part-b#0 |
+|         |                                                         |                                                                                |
--- TP_1.2B/app/src/main/java/com/example/p12b/MainActivity.java
+++ TP_1.2B/app/src/main/java/com/example/p12b/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.p12b;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,12 +13,18 @@ import org.w3c.dom.Text;
 public class MainActivity extends AppCompatActivity {
     private int mCount = 0;
     private TextView mShowCount;
+//    final Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = (TextView)  findViewById(R.id.show_count);
+
+        final Button button = (Button) findViewById(R.id.count);
+
+//        Log.i(button.toString());
+//        Log.i("1",Integer.toString(mCount));
     }
 
     public void showToast(View view) {
@@ -28,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
     public void countUp(View view){
         mCount++;
 
+//        Log.i(mCount);
+
+//        Log.i("1",Integer.toString(mCount));
         if(mShowCount != null){
             mShowCount.setText(Integer.toString(mCount));
         }
